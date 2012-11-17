88cf969429349869a2d6765e2f6eeb37195dd44c
Keita Urashima <ursm@ursm.jp>
2012-11-17 20:31:50 +0900

    bundle gem

---
+++ lib/active_record-denormalize.rb
@@ -0,0 +1,7 @@
+require "active_record-denormalize/version"
+
+module ActiveRecord
+  module Denormalize
+    # Your code goes here...
+  end
+end
---
+++ lib/active_record-denormalize/version.rb
@@ -0,0 +1,5 @@
+module ActiveRecord
+  module Denormalize
+    VERSION = "0.0.1"
+  end
+end
